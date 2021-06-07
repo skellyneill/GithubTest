659898fa3bf09f7d7fbbda51f309bff75400cd37
Neill <skellyneill@gmail.com>
2021-06-07 21:40:27 +1200

    Changed console text

--- GithubTest/Program.cs
+++ GithubTest/Program.cs
@@ -6,7 +6,7 @@ namespace GithubTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Branch 1");
         }
     }
 }
