5f60c3429799b9bcee62b0dba380766db7cc4afe
Neill <skellyneill@gmail.com>
2021-06-07 21:25:24 +1200

    First commit

---
+++ GithubTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
